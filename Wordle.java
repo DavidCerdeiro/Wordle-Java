636d869abd8d8a037f8e5e1f303350a1e3e907ae
daviddcer22 <davidcergall22@gmail.com>
2025-03-06 17:42:11 +0100

    Código completado

--- Wordle.java
+++ Wordle.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-
 public class Wordle{
+    //Constante con número máximo de intentos
+    private static final int MAXINTENTOS = 5;
     public static void main(String[] args){
         //Si el nombre del fichero no es recibido a traves de los argumentos, suponemos que es diccionario.txt
         String nombreFichero = args.length == 0 ? "diccionario.txt": args[0];
@@ -14,12 +15,61 @@ public class Wordle{
             palabras = lecturaFichero(nombreFichero);
 
             if(!palabras.isEmpty()){
-                Random random = new Random();
-                //Nos aseguramos que el número generado esté dentro del rango de palabras disponibles
-                String palabraAleatoria = palabras.get(random.nextInt(palabras.size()));                
+                Random random;
+                Scanner scanner = new Scanner(System.in);
+                int longPal;         
+                int numIntentos;
+                int continuar;
+                boolean acierto;
+                boolean otraPartida = true;
+                String palIntroducida;
+                String resuelto;
+                //Presentación del juego
+                System.out.println("¡Bienvenido a Wordle!");
+                System.out.println("En esta humilde imitación del famoso juego, tendrás 5 oportunidades para adivinar la palabra.");
+                System.out.println("He aquí un ejemplo práctico del funcionamiento:");
+                System.out.println("Si la palabra a resolver es COSA y se introduce POCA, el resultado sería el siguiente: _O*A\n");
+                System.out.println("-Si la letra introducida es correcta y se encuentra en su posición, se mostrará tal cual.\n");
+                System.out.println("-Si la letra introducida es correcta pero no su posición, se mostrará el símbolo *.\n");
+                System.out.println("-Si la letra introducida no se encuentra en la palabra a resolver se mostrará _.\n");
+                System.out.println("***¡COMENCEMOS!***");
+                while(otraPartida){
+                    random = new Random();
+                    //Nos aseguramos que el número generado esté dentro del rango de palabras disponibles
+                    String palabraAdivinar = palabras.get(random.nextInt(palabras.size()));
+                    resuelto = "";
+                    longPal = palabraAdivinar.length();
+                    numIntentos = MAXINTENTOS;
+                    acierto = false;
+                    //Al principio obviamente la cadena resuelto estará vacía, es decir, rellena de _
+                    for(int i = 0; i<longPal; i++){
+                        resuelto += "_";
+                    }                
+                    while(!acierto && numIntentos!=0){
+                        System.out.println("Número de intentos restantes: "+numIntentos);
+                        System.out.println("Resolución: " + resuelto);
+                        //Nos aseguramos que la palabra introducida sea de la misma longitud que la palabra a adivinar
+                        do{
+                            System.out.println("Introduzca por favor, una palabra de "+longPal + " caracteres, en caso contrario, se le volverá a solicitar.");
+                            palIntroducida = scanner.nextLine();
+                        }while(palIntroducida.length() != longPal);
+                        //Si la palabra es correcta, salimos del bucle, si no, comprobamos los aciertos y seguimos
+                        if(palabraAdivinar.equals(palIntroducida)){
+                            acierto = true;
+                        }else{
+                            numIntentos--;
+                            resuelto = comprobarAciertos(palIntroducida, palabraAdivinar, longPal);
+                        }
+                    }
+                    System.out.println(acierto ?"***¡FELICIDADES, HAS GANADO!***" : "***NO LO HAS PODIDO ACERTAR :(*** /n La palabra correcta era: "+palabraAdivinar);
+                    System.out.println("¿Deseas realizar otra partida?");
+                    System.out.println("**Introduzca 1 para continuar.");
+                    System.out.println("*En caso contrario, cualquier otro número.");
+                    continuar = scanner.nextInt();
+                    if(continuar != 1) otraPartida = false;
+                }
                 
-                
-                System.out.println("Palabra aleatoria: " + palabraAleatoria);
+               
             }else{
                 System.out.println("El archivo no contiene palabras.");
             }
@@ -47,5 +97,21 @@ public class Wordle{
         scanFile.close();
 
         return palabras;
+    }
+
+    //Función para comprobar los aciertos y errores de la palabra introducida respecto a la que se busca adivinar
+    public static String comprobarAciertos(String palIntroducida, String palabraAdivinar, int longPal){
+        String aciertos = "";
+        for(int i = 0; i<longPal; i++){
+            if(palIntroducida.charAt(i) == palabraAdivinar.charAt(i)){
+                aciertos += palIntroducida.charAt(i);
+            }else if(palabraAdivinar.indexOf(palIntroducida.charAt(i)) != -1){
+                aciertos += "*";
+            }else{
+                aciertos += "_";
+            }
+        }
+
+        return aciertos;
     }
 }
